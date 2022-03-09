548a6ca4c7dd6f7a8a50dda67c7271910549b797
dbBurzum <khalidsr@code.edu.az>
2022-03-10 01:10:41 +0400

    Add project files.

---
+++ seasons ifelse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace seasons_ifelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Ayı daxil edin: ");
+                string a = Console.ReadLine();
+                string b = "yanvar";
+                string c = "fevral";
+                string d = "mart";
+                string e = "aprel";
+                string f = "may";
+                string g = "iyun";
+                string h = "iyul";
+                string j = "avqust";
+                string k = "sentyabr";
+                string l = "oktyabr";
+                string m = "noyabr";
+                string n = "dekabr";
+                 if (a == b || a == c || a == n)
+                {
+                    Console.WriteLine($"{a} ayının daxil olduğu sezon: Qış");
+                }
+                else if (a == d || a == e || a == f)
+                {
+                    Console.WriteLine($"{a} ayının daxil olduğu sezon: Yaz");
+                }
+                else if (a == g || a == h || a == j)
+                {
+                    Console.WriteLine($"{a} ayının daxil olduğu sezon: Yay");
+                }                
+                else if (a == k || a == l || a == m)
+                {
+                    Console.WriteLine($"{a} ayının daxil olduğu sezon: Payız");
+                }
+                else
+                {
+                    Console.WriteLine($"{a} ay deyil");
+                }
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ seasons/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace seasons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Ayı daxil edin: ");
+                string a = Console.ReadLine();
+                switch (a)
+                {
+                    case "yanvar":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Qış");
+                        break;
+                    case "fevral":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Qış");
+                        break;
+                    case "dekabr":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Qış");
+                        break;
+                    case "mart":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Yaz");
+                        break;
+                    case "aprel":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Yaz");
+                        break;
+                    case "may":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Yaz");
+                        break;
+                    case "iyun":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Yay");
+                        break;
+                    case "iyul":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Yay");
+                        break;
+                    case "avqust":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Yay");
+                        break;
+                    case "sentyabr":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Payız");
+                        break;
+                    case "oktyabr":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Payız");
+                        break;
+                    case "noyabr":
+                        Console.WriteLine($"{a} ayının aid olduğu sezon: Payız");
+                        break;
+                    default:
+                        Console.WriteLine($"{a} ay deyil");
+                        break;
+
+                }
+            }
+  
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
