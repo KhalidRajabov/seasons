58c6d2b2b3a99d7e8c9611d49bb0321d36846b61
dbBurzum <khalidsr@code.edu.az>
2022-03-10 16:53:51 +0400

    added: Arrays with for loop

---
+++ seasons array+for loop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace seasons_array_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Ay daxil edin: ");
+                string[] winter = { "dekabr", "yanvar", "fevral" };
+                string[] spring = { "mart", "aprel", "may" };
+                string[] summer = { "iyun", "iyul", "avqust" };
+                string[] autumn = { "sentyabr", "oktyabr", "noyabr" };
+                string a = Console.ReadLine();
+                for (int i = 0; i < winter.Length; i++)
+                {
+                    if (a == winter[i])
+                    {
+                        Console.WriteLine($"{a} ayının daxil olduğu sezon: Qış");
+                    }
+                }
+                for (int j = 0; j < spring.Length; j++)
+                {
+                    if (a == spring[j])
+                    {
+                        Console.WriteLine($"{a} ayının daxil olduğu sezon: Yaz");
+                    }
+                }
+                for (int k = 0; k < summer.Length; k++)
+                {
+                    if (a == summer[k])
+                    {
+                        Console.WriteLine($"{a} ayının daxil olduğu sezon: Yay");
+                    }
+                }
+                for (int l = 0; l < autumn.Length; l++)
+                {
+                    if (a == autumn[l])
+                    {
+                        Console.WriteLine($"{a} ayının daxil olduğu sezon: Payız");
+                    }
+                }
+            }
+     
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+    }
+}
